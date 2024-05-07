5a22b56951ef8cb2600cb3e62d859dc249f61e1d
otondonicolas <n.otondo.pineyro@davinci.edu.ar>
2024-05-07 12:02:41 -0300

    proyecto inicial

---
+++ app/src/main/java/com/example/parcial_uno_app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.parcial_uno_app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout navigationDrawer;
+
+    @SuppressLint({"MissingInflatedId", "ClickableViewAccessibility"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Obtén la referencia al TextView del saludo
+        TextView greetingTextView = findViewById(R.id.greeting);
+
+        // Define el nombre del usuario
+        String userName = "Nicolas"; // Puedes obtener este valor de las preferencias del usuario o de cualquier otra fuente.
+
+        // Establece el texto del saludo con el nombre del usuario
+        greetingTextView.setText(getString(R.string.greeting, userName));
+
+    }
+
+}
